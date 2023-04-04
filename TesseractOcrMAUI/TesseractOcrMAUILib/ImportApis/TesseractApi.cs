b5b7a71852db783ec756b2d2c89a624bbb25607e
matikkaeditorinkaantaja <matikkaeditorinkaantaja@gmail.com>
2023-04-04 15:16:49 +0300

    Remove old dlls and change APIs
    - Remove old windows dlls from platform folder
    - Change API methods to public, classes are still internal, so still should not access them from outside

--- TesseractOcrMAUI/TesseractOcrMAUILib/ImportApis/TesseractApi.cs
+++ TesseractOcrMAUI/TesseractOcrMAUILib/ImportApis/TesseractApi.cs
@@ -18,13 +18,13 @@ internal sealed partial class TesseractApi
 
     [LibraryImport(DllName, EntryPoint = "TessBaseAPICreate")]
     [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
-    internal static partial IntPtr CreateApi();
+    public static partial IntPtr CreateApi();
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIDelete")]
-    internal static extern void DeleteApi(HandleRef handle);
+    public static extern void DeleteApi(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIInit4", CharSet = StrEncoding)]
-    internal extern static int BaseApi4Init(HandleRef handle, string datapath, string language,
+    public extern static int BaseApi4Init(HandleRef handle, string datapath, string language,
         int mode, string[] configs, int configLength, string[] optionNames,
         string[] optionValues, UIntPtr optionsSize, bool setOnlyNonDebugParams);
 
@@ -32,77 +32,77 @@ internal sealed partial class TesseractApi
     // In baseapi.cpp in line 388:
     // std::string datapath = data_size == 0 ? data: language;
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIInit5", CharSet = StrEncoding)]
-    internal extern static int BaseApi5Init(HandleRef handle, string datapath, int dataSize,
+    public extern static int BaseApi5Init(HandleRef handle, string datapath, int dataSize,
         string language, int mode, string[] configs, int configSize, string[] optionNames,
         string[] optionValues, UIntPtr optionsSize, bool setOnlyNonDebugParams);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetDatapath")]
-    internal static extern string GetDataPath(HandleRef handle);
+    public static extern string GetDataPath(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetImage2")]
-    internal static extern void SetImage(HandleRef handle, HandleRef pixHandle);
+    public static extern void SetImage(HandleRef handle, HandleRef pixHandle);
     
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetUTF8Text", CharSet = StrEncoding)]
-    internal static extern string GetUTF8Text(HandleRef handle);
+    public static extern string GetUTF8Text(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIAllWordConfidences")]
-    internal static extern int[] GetConfidences(HandleRef handle);
+    public static extern int[] GetConfidences(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIClear")]
-    internal static extern void Clear(HandleRef handle);
+    public static extern void Clear(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetSourceYResolution")]
-    internal static extern int GetSourceResolution(HandleRef handle);
+    public static extern int GetSourceResolution(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetOutputName", CharSet = StrEncoding)]
-    internal static extern void SetOutputName(HandleRef handle, string name);
+    public static extern void SetOutputName(HandleRef handle, string name);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetInputName", CharSet = StrEncoding)]
-    internal static extern void SetInputName(HandleRef handle, string name);
+    public static extern void SetInputName(HandleRef handle, string name);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetInputName", CharSet = StrEncoding)]
-    internal static extern string GetInputName(HandleRef handle);
+    public static extern string GetInputName(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetInputImage", CharSet = StrEncoding)]
-    internal static extern string SetInputImage(HandleRef handle);
+    public static extern string SetInputImage(HandleRef handle);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetPageSegMode")]
-    internal static extern void SetPageSegmentationMode(HandleRef handle, PageSegmentationMode mode);
+    public static extern void SetPageSegmentationMode(HandleRef handle, PageSegmentationMode mode);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIRecognize")]
-    internal static extern int Recognize(HandleRef handle, HandleRef monitor);
+    public static extern int Recognize(HandleRef handle, HandleRef monitor);
     
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetThresholdedImage")]
-    internal static extern IntPtr GetThresholdedImage(HandleRef handle);
+    public static extern IntPtr GetThresholdedImage(HandleRef handle);
 
     [LibraryImport(DllName, EntryPoint = "TessVersion", StringMarshalling = StringMarshalling.Custom, 
         StringMarshallingCustomType = typeof(System.Runtime.InteropServices.Marshalling.AnsiStringMarshaller))]
     [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
-    internal static partial IntPtr GetVersion();
+    public static partial IntPtr GetVersion();
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetDebugVariable", CharSet = StrEncoding)]
-    internal static extern int SetDebugVariable(HandleRef handle, string name, string value);
+    public static extern int SetDebugVariable(HandleRef handle, string name, string value);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPISetVariable", CharSet = StrEncoding)]
-    internal static extern int SetVariable(HandleRef handle, string name, string value);
+    public static extern int SetVariable(HandleRef handle, string name, string value);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetIntVariable", CharSet = StrEncoding)]
-    internal static extern int GetIntVariable(HandleRef handle, string name, out int value);
+    public static extern int GetIntVariable(HandleRef handle, string name, out int value);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetDoubleVariable", CharSet = StrEncoding)]
-    internal static extern int GetDoubleVariable(HandleRef handle, string name, out double value);
+    public static extern int GetDoubleVariable(HandleRef handle, string name, out double value);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetBoolVariable", CharSet = StrEncoding)]
-    internal static extern int GetBoolVariable(HandleRef handle, string name, out int value);
+    public static extern int GetBoolVariable(HandleRef handle, string name, out int value);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIGetBoolVariable", CharSet = StrEncoding)]
-    internal static extern string GetStringVariable(HandleRef handle, string name);
+    public static extern string GetStringVariable(HandleRef handle, string name);
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIPrintVariablesToFile", CharSet = StrEncoding)]
-    internal static extern int PrintVariablesToFile(HandleRef handle, string fileName);
+    public static extern int PrintVariablesToFile(HandleRef handle, string fileName);
 
 
     [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessBaseAPIMeanTextConf")]
-    internal static extern int GetMeanConfidence(HandleRef handle);
+    public static extern int GetMeanConfidence(HandleRef handle);
  
 }
