e2560546688e19e6516e132664f1fa6a88b44ef8
matikkaeditorinkaantaja <matikkaeditorinkaantaja@gmail.com>
2024-05-12 12:37:19 +0300

    Update PageSegmentationMode.cs

--- TesseractOcrMaui/Enums/PageSegmentationMode.cs
+++ TesseractOcrMaui/Enums/PageSegmentationMode.cs
@@ -1,25 +1,83 @@
 ﻿namespace TesseractOcrMaui.Enums;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 /// <summary>
 /// Possible page layout analysis modes
 /// </summary>
 public enum PageSegmentationMode
 {
-    OsdOnly,
-    AutoOsd,
-    AutoOnly,
-    Auto,
-    SingleColumn,
-    SingleBlockVertText,
-    SingleBlock,
-    SingleLine,
-    SingleWord,
-    CircleWord,
-    SingleChar,
-    SparseText,
-    SparseTextOsd,
-    RawLine,
-    Count
+    /// <summary>
+    /// Orientation and script detection (OSD) only.
+    /// </summary>
+    OsdOnly = 0,
+
+    /// <summary>
+    /// Automatic page segmentation with OSD.
+    /// </summary>
+    AutoOsd = 1,
+
+    /// <summary>
+    /// Automatic page segmentation, but no OSD, or OCR.
+    /// </summary>
+    AutoOnly = 2,
+
+    /// <summary>
+    /// Fully automatic page segmentation, but no OSD. (Default)
+    /// </summary>
+    Auto = 3,
+
+    /// <summary>
+    /// Assume a single column of text of variable size.
+    /// </summary>
+    SingleColumn = 4,
+
+    /// <summary>
+    /// Assume a single uniform block of vertically aligned text.
+    /// </summary>
+    SingleBlockVertText = 5,
+
+    /// <summary>
+    /// Assume a single uniform block of text.
+    /// </summary>
+    SingleBlock = 6,
+
+    /// <summary>
+    /// Treat the image as a single text Iine.
+    /// </summary>
+    SingleLine = 7,
+
+    /// <summary>
+    /// Treat the image as a single word.
+    /// </summary>
+    SingleWord = 8,
+
+    /// <summary>
+    /// Treat the image as a single word in a circle.
+    /// </summary>
+    CircleWord = 9,
+
+    /// <summary>
+    /// Treat the image as a single character.
+    /// </summary>
+    SingleChar = 10,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    SparseText = 11,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    SparseTextOsd = 12,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    RawLine = 13,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    Count = 14
 }
